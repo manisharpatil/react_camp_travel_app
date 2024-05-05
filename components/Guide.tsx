f7d332f92e899082857661ce4b2eb1822760c2c5
Manisha Patil <manisharpatil6@gmail.com>
2024-05-05 16:56:22 +0530

    feat: added guide section completely

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -5,7 +5,7 @@ const Guide = () => {
   return (
     <section className="flexCenter flex-col">
       <div
-        className="boder-2 border-red-500 padding-container
+        className="padding-container
       max-container w-full pb-24"
       >
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
@@ -29,7 +29,7 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+      <div className="flexCenter max-container relative w-full">
         <Image
           src="/boat.png"
           alt="boat"
@@ -40,7 +40,29 @@ const Guide = () => {
         <div
           className="absolute flex bg-white py-8 pl-5 pr-7 gap-3
         rounded-3xl border shadow-md md:left-[5%] lg:top-20"
-        ></div>
+        >
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Pawana Camping</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Tikona Fort</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
